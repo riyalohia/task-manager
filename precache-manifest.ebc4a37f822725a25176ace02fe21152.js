28c9d3bb856aaf7ee3dfb705f368129b3b97f441
Riya Lohia <riyalh1997@gmail.com>
2020-06-06 22:36:09 +0530

    Updates

---
+++ precache-manifest.ebc4a37f822725a25176ace02fe21152.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122ad64d59c0b046f564926ed60ab4c8",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "d1aeb3b82e3a3568d6e3",
+    "url": "/task-manager/static/css/2.a0dd8689.chunk.css"
+  },
+  {
+    "revision": "12f750d57a56bab84b81",
+    "url": "/task-manager/static/css/main.5493934e.chunk.css"
+  },
+  {
+    "revision": "d1aeb3b82e3a3568d6e3",
+    "url": "/task-manager/static/js/2.05fc260a.chunk.js"
+  },
+  {
+    "revision": "269a51ec685a244a5c79d81f4a6003f2",
+    "url": "/task-manager/static/js/2.05fc260a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f750d57a56bab84b81",
+    "url": "/task-manager/static/js/main.f87ca7ea.chunk.js"
+  },
+  {
+    "revision": "c877b8986ef07cc1606b",
+    "url": "/task-manager/static/js/runtime-main.83d32b0f.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/task-manager/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/task-manager/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/task-manager/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/task-manager/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
